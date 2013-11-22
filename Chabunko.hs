5af80136f8da9229d9b097d7dbae3112b3b70794
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-11-22 07:41:17 +0000

    Include GLPv2 header in the source file

--- Chabunko.hs
+++ Chabunko.hs
@@ -1,6 +1,18 @@
+-- Copyright (C) 2013  Shou
 
--- Copyright Shou, 2013
--- Licensed under the GNU GPLv2
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 -- TODO
 --  - Separate metadata file per user
@@ -120,7 +132,7 @@ ircOut req = do
     conv (x, y) = (T.decodeUtf8 x, maybe "" T.decodeUtf8 y)
     esc = T.replace "<" "&lt;" . T.replace ">" "&gt;" . T.replace "&" "&amp;"
 
--- sink = Sink 
+-- sink = Sink
 -- parseRequestBody sink req
 ircIn req = do
     liftIO $ print $ queryString req
@@ -242,4 +254,3 @@ runIRC = do
 
 main :: IO ()
 main = runIRC >> runSettings settings app
-
